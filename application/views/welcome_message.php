76abe7aeb899b0fceae44d32e4ce58ccf399dcc6
giriputraadhittana <girput01@icloud.com>
2019-12-02 00:12:17 +0700

    Initial commit

---
+++ application/views/welcome_message.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<p>A function is triggered when the user releases a key in the input field. The function transforms the character to upper case.</p>
+Enter your name: <input type="text" id="fname" onkeyup="myFunction(event)">
+
+<script type='text/javascript'>
+
+function myFunction(evt) {
+	if(evt.which==13)
+	{
+		alert("Giri Putra Adhittana");
+		console.log("Hello World");
+		fefefefe
+	}
+}
+</script>
+
+</body>
+</html>
